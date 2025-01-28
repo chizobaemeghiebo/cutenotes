ed2254683c59c29132ba59b19f047f70e2c47197
Chizoba Emeghiebo <chizobaemeh09@gmail.com>
2025-01-28 10:47:19 +0100

    added mock backend to perform crud operations

--- src/components/NoteListings.jsx
+++ src/components/NoteListings.jsx
@@ -1,21 +1,48 @@
 import { FaCalendar } from "react-icons/fa";
-import notes from "../notes.json";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 
 const NoteListings = () => {
-  console.log(notes);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/notes");
+        // get the data
+        const data = await res.json();
+        // set the data to the state
+        setNotes(data);
+      } catch (error) {
+        // log the error
+        console.log("Error fetching data", error);
+      } finally {
+        // set loading to false because finally runs eitherway
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
     <div className=" flex flex-col md:grid md:grid-cols-2 gap-4 lg:grid-cols-3">
-      {notes.map((note) => (
-        <Card>
-          <h3>{note.title}</h3>
-          <p>{note.content}</p>
-          <span className="text-gray-500 text-sm flex items-center">
-            <FaCalendar /> {note.date}
-          </span>
-        </Card>
-      ))}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {notes.map((note) => (
+            <Card>
+              <h3 className="font-title">{note.title}</h3>
+              <p className="font-note">{note.content}</p>
+              <span className="text-gray-500 text-sm flex gap-2 items-center">
+                <FaCalendar /> {note.date}
+              </span>
+            </Card>
+          ))}
+        </>
+      )}
     </div>
   );
 };
---
+++ src/pages/EditNote.jsx
@@ -0,0 +1,5 @@
+const EditNote = () => {
+  return <div>Edit Notes Here</div>;
+};
+
+export default EditNote;
