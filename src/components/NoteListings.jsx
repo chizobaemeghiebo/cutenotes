71bfeedc70f4ee0623534c74e1feb21873802c54
Chizoba Emeghiebo <chizobaemeh09@gmail.com>
2025-03-21 22:39:23 +0100

    added display for when there are no notes and UI change

--- src/components/NoteListings.jsx
+++ src/components/NoteListings.jsx
@@ -2,11 +2,10 @@ import { FaCalendar } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import EditIcon from "../assets/images/edit.svg";
-import TrashIcon from "../assets/images/trash.svg";
 import Spinner from "./Spinner";
 import Card from "./Card";
 
-const NoteListings = ({ deleteNote }) => {
+const NoteListings = () => {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -38,7 +37,14 @@ const NoteListings = ({ deleteNote }) => {
         <Spinner loading={loading} />
       ) : (
         // show the spinner if loading is true
-        <div className=" flex flex-col md:grid md:grid-cols-2 gap-4 lg:grid-cols-3">
+        <div className="flex flex-col md:grid md:grid-cols-2 gap-4 lg:grid-cols-3">
+          {/* show message if there are no notes */}
+          {notes.length === 0 && (
+            <div className="col-span-full m-auto text-gray-500">
+              No notes yet. Add a new note.
+            </div>
+          )}
+
           {notes.map((note) => (
             <Card key={note.id}>
               <div className="flex items-center justify-between">
@@ -47,9 +53,17 @@ const NoteListings = ({ deleteNote }) => {
                   <img src={EditIcon} alt="edit note" />
                 </Link>
               </div>
-              <p className="font-note">{note.content}</p>
+              {note.content.slice(0, 200)}
+              {note.content.length > 200 && (
+                <Link
+                  to={`/notes/${note.id}`}
+                  className="text-pink-800 text-sm"
+                >
+                  Read more
+                </Link>
+              )}
 
-              <span className="text-xs text-slate-900 flex gap-2 items-center">
+              <span className="absolute bottom-4 right-4 text-xs text-slate-900 flex gap-2">
                 <FaCalendar /> {note.date}
               </span>
             </Card>
